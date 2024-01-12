e0de7e9672deb541c1ea6d3428dd255213ad89ba
liuyp2080 <78184640+liuyp2080@users.noreply.github.com>
2024-01-12 13:01:36 +0800

    Add files via upload

--- app_guider.py
+++ app_guider.py
@@ -63,8 +63,8 @@ if __name__ == '__main__':
     st.sidebar.subheader('📚机器学习知识类')
     st.sidebar.write('目前并没有大面积收集机器学习的知识，而是收录了一些独特的经验和体会，比如，DCA分析的注意事项等')
     #导航机器人
-    st.write("api_key", st.secrets["API_key"])
-    st.write("secret_key", st.secrets["secret_key"])
+    api_key=st.secrets["API_key"]
+    secret_key=st.secrets["secret_key"]
     if "messages" not in st.session_state:
         st.session_state["messages"] = [{"role": "assistant", "content": "你好！我收藏我很多的医学APP，你可以问我索要APP的地址进行体验；我还有许多构建APP的体会，你在网络上没有找得到的可以问我哦"}]
 
